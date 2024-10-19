ef2d12dc85411e76da2a79b06cd3817739196bba
Syahrulramadn <syarulramadhan00@gmail.com>
2024-10-19 14:51:29 +0700

    Update

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient
-from datetime import datetime
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+from datetime import datetime
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
